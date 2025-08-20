6261e9ad26587cff72477e2058c1360d9d01f016
kasrababaa <Kaskou1122@skola.goteborg.se>
2025-08-20 22:44:42 +0200

    hemma
    uppgift 1 påbörjan

--- projekt-1/test.php
+++ projekt-1/test.php
@@ -1,3 +1,6 @@
+<?php
+    $text = "Type shit dawg";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,9 +11,26 @@
 </head>
 <body>
 <?php
-    echo "<p>Hej<p>";
+    echo "<p>Hej</p>";
     echo "<p>Hej igen<p>";
     echo "<p>YO<p>";
+    echo "<p>" . $text . "</p>";
 ?>
+<p>Vad heter du akhi?</p>
+<form action="" method="get">
+    <input type="text" name="secret" id="">
+    <input type="submit" value="Tryck yäni">
+</form>
+
+<p>
+    <?php
+        if (isset($_GET["secret"])) {
+            if ("secret" == "pizza") {
+                echo "<p>Pizza är gott</p>";
+            }
+            
+        }
+    ?>
+</p>
 </body>
 </html>
