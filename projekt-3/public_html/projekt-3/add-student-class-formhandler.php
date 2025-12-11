108b8eb9c6777bf2dc13487e6f9a2e6a55d40b7a
kasrababaa <Kaskou1122@skola.goteborg.se>
2025-12-11 20:05:54 +0100

    funkar inte for some reason_commit

--- projekt-3/public_html/projekt-3/add-student-class-formhandler.php
+++ projekt-3/public_html/projekt-3/add-student-class-formhandler.php
@@ -1,6 +1,8 @@
 <?php
 
-$checkStudentStmt = $pdo->prepare("SELECT StudentsClasses WHERE StudentID = :STudentId AND ClassId = :ClassId");
+require_once("../../projekt-3-app.php");
+
+$checkStudentStmt = $pdo->prepare("SELECT * FROM StudentsClasses WHERE StudentId = :StudentId AND ClassId = :ClassId");
 $checkStudentStmt->execute([
     "StudentId" => $_POST['student'],
     "ClassId" => $_POST['class']
@@ -8,3 +10,17 @@ $checkStudentStmt->execute([
 ]);
 
 $checkStudentResult = $checkStudentStmt->fetchAll(PDO::FETCH_ASSOC);
+
+if ($checkStudentResult != false) {
+    echo "Eleven läser redan kursen!";
+    exit;
+}
+
+$addStudentClassStmt = $pdo -> prepare("INSERT INTO StudentsClasses (StudentId, ClassId) Values (:StudentId , :ClassID)");
+
+$addStudentClassStmt -> execute ([
+    "StudentId" => $_POST['student'],
+    "ClassId" => $_POST['class']
+]);
+
+header ("Loacation : student.php?id=" . $_POST['student']);
